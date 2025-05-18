838d4d1e32447da301d7a17f044ec1aca2281347
Rael <emilyekos@gmail.com>
2025-05-19 03:23:56 +0900

    feat: 페이지 라우팅 실습 및 getLayout 적용

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,6 +1,32 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ReactNode } from "react";
+// import Link from "next/link";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({ Component, pageProps }: AppProps & { Component: NextPageWithLayout }) {
+  // const router = useRouter();
+
+  // const onClickButton = () => {
+  //   router.push("/test");
+  // };
+
+  // useEffect(() => {
+  //   router.prefetch("/test");  // 명시적으로 프리페칭
+  // }, []);
+
+  const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page);
+  
+  return (
+    <GlobalLayout>
+      {getLayout(<Component {...pageProps} />)}
+    </GlobalLayout>
+  );
 }
