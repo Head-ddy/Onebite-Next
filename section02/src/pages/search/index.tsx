e2056dac7d79db3314caf43f0a501a69b862f6f9
Rael <emilyekos@gmail.com>
2025-05-19 04:47:22 +0900

    feat: 도서 검색 기능 구현

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,9 +1,24 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
 
-export default function Page() {
+export const getServerSideProps = async (context : GetServerSidePropsContext) => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+  // 서버에서만 실행되며, 클라이언트에서는 실행되지 않음
+
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+
+  return {
+    props: {
+      books,
+    }
+  } 
+}
+
+export default function Page({books}: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
   return (
     <div>
