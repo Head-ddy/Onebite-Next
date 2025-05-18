2cd92dade5c3793b638d6a8a6a2f3371436d24e3
Rael <emilyekos@gmail.com>
2025-05-19 06:10:22 +0900

    docs: 오타 수정

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -25,7 +25,7 @@ export default function Page({books}: InferGetServerSidePropsType<typeof getServ
     <div>
       <Head>
         <title>한입북스 - 검색결과</title>
-        <meta property='og:image' content="/thumnail.png" />
+        <meta property='og:image' content="/thumbnail.png" />
         <meta property='og:title' content="한입북스" />
         <meta property='og:description' content="한입북스에 등록된 도서들을 만나보세요" />
       </Head>
