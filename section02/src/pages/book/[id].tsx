ced7ea013c5c12ad96f4029fb89bf831d600653f
Rael <emilyekos@gmail.com>
2025-05-19 06:00:03 +0900

    docs: 코드 및 오타 수정

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -83,7 +83,12 @@ export default function Page({book}: InferGetServerSidePropsType<typeof getServe
 
   return (
     <>
-      
+      <Head>
+        <title>{title}</title>
+        <meta property='og:image' content={coverImgUrl} />
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
+      </Head>
       <div className={style.container}>
         <div className={style.cover_img_container} style={{ backgroundImage: `url(${coverImgUrl})` }}>
           <img src={coverImgUrl} alt={title} />
--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home({allBooks, recoBooks}: InferGetStaticPropsType<type
     <>
       <Head>
         <title>한입북스</title>
-        <meta property='og:image' content="/thumnail.png" />
+        <meta property='og:image' content="/thumbnail.png" />
         <meta property='og:title' content="한입북스" />
         <meta property='og:description' content="한입북스에 등록된 도서들을 만나보세요" />
       </Head>
